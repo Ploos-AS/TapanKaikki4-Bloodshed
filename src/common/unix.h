caced569588127dc172b8b18704b8503f168e40b
pos <pgousdal@users.noreply.github.com>
2026-09-13 13:12:55 +0200

    M1: add classic Amiga portability definitions

--- src/common/unix.h
+++ src/common/unix.h
@@ -5,16 +5,25 @@
 #define __unix__
 #endif
 
-#ifdef __unix__
+#if defined(__unix__) || defined(AMIGA)
 
+#ifndef EXPORT
 #define EXPORT
+#endif
+
+#ifdef AMIGA
+#define NORETURN __attribute__((noreturn))
+#else
 #define NORETURN
+#endif
 
 /* Some functions and names that differ on windows and other platforms */
 #define EF_STAT stat
 #define EF_SNPRINTF snprintf
 #define EF_GETCWD getcwd
+#ifndef AMIGA
 #define EF_SDLWINDOW info.x11.window /* window in SDL_SysWMinfo structure */
+#endif
 
 #define DIR_SEPARATOR '/'
 
@@ -38,7 +47,8 @@ inline void ChangeDir(const char* str)
 }
 
 inline char* strupr(char *buf)
-{	int len=strlen(buf);
+{
+	int len=strlen(buf);
 	
 	for (int a=0;a<len;a++)
 	{
@@ -48,7 +58,8 @@ inline char* strupr(char *buf)
 }
 
 inline char* strlwr(char *buf)
-{	int len=strlen(buf);
+{
+	int len=strlen(buf);
 	
 	for (int a=0;a<len;a++)
 	{
@@ -58,7 +69,8 @@ inline char* strlwr(char *buf)
 }
 
 inline char *strset(char *buf,char fill)
-{	int len=strlen(buf);
+{
+	int len=strlen(buf);
 	
 	for (int a=0;a<len;a++)
 	{
@@ -67,7 +79,6 @@ inline char *strset(char *buf,char fill)
 	return buf;
 }
 
-#endif // __unix__
+#endif // __unix__ || AMIGA
 
 #endif // __UNIX_H
-
