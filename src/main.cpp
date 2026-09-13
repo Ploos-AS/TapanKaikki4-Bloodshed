39a0aa8965643a0d2535a1a930c39d3451aa49e6
pos <pgousdal@users.noreply.github.com>
2026-09-13 14:08:11 +0200

    M3: record Amiga splash and app initialization milestones

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,26 @@
 
 #include "common/files.h"
 
+#include <stdio.h>
+
 int abnormal_exit=1;
+
+#ifdef AMIGA
+static void amiga_m3_marker(const char* name)
+{
+	char path[160];
+	if (snprintf(path, sizeof(path), "PROGDIR:save/%s", name) <= 0)
+		return;
+	FILE* f = fopen(path, "w");
+	if (f)
+	{
+		fputs("1\n", f);
+		fclose(f);
+	}
+}
+#else
+static void amiga_m3_marker(const char*) {}
+#endif
 
 void ExitHandler()
 {
@@ -25,12 +44,15 @@ int main(int argc,char *argv[])
 	CGameApp* GGameApp = NULL;
 	atexit(ExitHandler);
 	chdir(getdatapath(".").c_str());
+	amiga_m3_marker("m3-main-started.txt");
 
 	try
 	{
 		// TODO: CONFIGFILE
 		CSplash::ShowSplash("efps/splash.bmp",KIconFile.c_str(),KWindowCaption);
+		amiga_m3_marker("m3-splash-ok.txt");
 		GGameApp = new CGameApp(KIconFile.c_str(),KWindowCaption);
+		amiga_m3_marker("m3-app-ok.txt");
 #ifdef _DEBUG
 		GGameApp->SelfTest();
 #endif
@@ -57,4 +79,3 @@ int main(int argc,char *argv[])
 	abnormal_exit=0;
 	return 0;
 }
-
